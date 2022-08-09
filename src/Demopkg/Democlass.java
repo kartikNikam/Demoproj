480c7ed5431db42c0442df0c9d2be9c51f4ec4c3
kartikNikam <kartiknikam8181@gmail.com>
2022-08-09 10:59:00 +0530

    commit1

--- src/Demopkg/Democlass.java
+++ src/Demopkg/Democlass.java
@@ -5,8 +5,40 @@ public class Democlass {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World");
-		System.out.println("Anebal Solution,Pune");
+		
+		// byte bytename = 127;
+		// byte = 1 byte / 8 bits = +127 - -128  2^8 = 256
+		byte a =127;
+		
+		//short
+		// short = 2 bytes or 16bits +32767 - -32768
+		short sh = 32767;
+		
+		//integer = 4 bytes or 32 bits 2147483647  -21474883648
+		
+		int i = 200;
+		
+		// long = 8 bytes or 64 bits 2^63-1  - 2^63
+		long q = 1000000;
+		
+		// float = 4bytes  -3.4*10^38 to 10^38
+		
+		float f = 1.2666666355f;
+		double d = 1.3099888877774777777378888883;
+		
+		// boolean choice 2  true / false
+		
+		boolean b = true;
+		boolean b1 = false;
+		
+		// char = 2 bytes
+		
+		char ch = 's';
+		char as ='$';
+		
+		
+		
+		
 
 	}
 
