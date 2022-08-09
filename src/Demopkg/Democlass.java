a4fd34987e3819d534708a61207e0c9cf6f47b74
kartikNikam <kartiknikam8181@gmail.com>
2022-08-09 10:06:20 +0530

    commit

---
+++ src/Demopkg/Democlass.java
@@ -0,0 +1,13 @@
+package Demopkg;
+
+public class Democlass {
+	
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World");
+		System.out.println("Anebal Solution,Pune");
+
+	}
+
+}
