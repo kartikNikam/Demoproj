480b7602ba32fda67287f27b2492a1464f9b16bb
kartikNikam <kartiknikam8181@gmail.com>
2022-08-24 20:04:50 +0530

    b1

---
+++ src/Demopkg/ControlStmts.java
@@ -0,0 +1,62 @@
+package Demopkg;
+
+public class ControlStmts {
+
+	public static void main(String[] args) {
+	
+		
+//	int marks = 70;
+//	
+//	if(marks>=50 && marks<60) {System.out.println("D grade");}
+//	else if(marks>=60 && marks<70) {System.out.println("C grade");}
+//	else if(marks>=70 && marks<80) {System.out.println("B grade");}
+//	else if(marks>=80) {System.out.println("Grade A");}
+//	else { System.out.println("incorrect input");}
+		
+	
+//	int no = 75;
+//	     if(no>=74) {
+//	    	  System.out.println("Valid no");
+//	    	 if(no>40) {
+//	    		 System.out.println("no is greater than 40");
+//	    	 }
+//	     }
+		
+		// switch() statements
+		/* switch(Expression){
+		 *  case 1:
+		 *  statement 1;
+		 *  break;
+		 *  case 2:
+		 *  statement 2;
+		 *  break ;
+		 * 
+		 * 
+		 * default:
+		 * default statement ;
+		 * }
+		 *  
+		 */
+	String month = "march";
+		int number  =2 ;
+		switch(number) {
+		case 1: System.out.println("this is no 1");
+		break;
+		case 2:System.out.println("this is no 2");
+		break;
+		case 3:System.out.println("this is no 3");
+		break;
+		default : System.out.println("invalid input");
+		
+		
+		
+		
+		}
+	  
+	     
+	     
+	     
+	     
+		}
+
+}
